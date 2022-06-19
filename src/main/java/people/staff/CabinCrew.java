cdb2d16baad55104e8165b7891c73954d20de154
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-06-19 18:24:44 +0100

    add relayMessage method to CabinCrew

--- src/main/java/people/staff/CabinCrew.java
+++ src/main/java/people/staff/CabinCrew.java
@@ -5,4 +5,8 @@ public class CabinCrew extends Staff{
     public CabinCrew(String name, RankType rank) {
         super(name, rank);
     }
+
+    public String relayMessage(String cabinCrewMessage) {
+        return cabinCrewMessage;
+    }
 }
--- src/test/java/CabinCrewTest.java
+++ src/test/java/CabinCrewTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import people.staff.CabinCrew;
+import people.staff.RankType;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewTest {
+
+    private CabinCrew cabinCrew;
+    @Before
+    public void before(){
+        cabinCrew = new CabinCrew("Jerry", RankType.RANK4);
+    }
+
+    @Test
+    public void crewCanRelayMessages(){
+        String cabinCrewMessage = "This is a reminder to all passengers that peanuts are strictly forbidden on this flight.";
+        assertEquals("This is a reminder to all passengers that peanuts are strictly forbidden on this flight.", cabinCrew.relayMessage(cabinCrewMessage));
+    }
 }
